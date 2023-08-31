b43feeec3d153f0a7936a406760d4b834fa5c118
Thomas Stassin <thomas.stassin@gsmproductions.com>
2023-08-31 13:20:09 +0200

    jump ok

--- Assets/#Project/Scripts/PlayerControl.cs
+++ Assets/#Project/Scripts/PlayerControl.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine.InputSystem;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
 public class PlayerControl : MonoBehaviour
 {
 
@@ -10,27 +12,51 @@ public class PlayerControl : MonoBehaviour
 
     public InputActionAsset actions;
     public float speed = 1f;
+    public float jumpForce = 500f;
     private InputAction xAxis;
+    private Rigidbody rb;
+
+    private bool isJumping;
 
     void Awake()
     {
         xAxis = actions.FindActionMap("CubeActionsMap").FindAction("XAxis");
+        rb = GetComponent<Rigidbody>();
     }
 
     void OnEnable()
     {
         actions.FindActionMap("CubeActionsMap").Enable();
+        actions.FindActionMap("CubeActionsMap").FindAction("Jump").performed += OnJump;
     }
 
     void OnDisable()
     {
         actions.FindActionMap("CubeActionsMap").Disable();
+        actions.FindActionMap("CubeActionsMap").FindAction("Jump").performed -= OnJump;
     }
 
     void Update()
     {
         MoveX();
         AutoMove();
+    }
+
+    private void OnJump(InputAction.CallbackContext context)
+    {
+        if (!isJumping)
+        {
+            rb.AddForce(Vector3.up * jumpForce);
+            isJumping = true;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
     }
 
     private void MoveX()
